5e0f9e338cef5499e09f8b2a6ac4d02772d3fdec
Jake Crowley <jakecrow@gmail.com>
2025-04-14 15:46:00 -0400

    add godot project as submodule

--- app/src/main/assets/checkers_board_top.gd
+++ app/src/main/assets/checkers_board_top.gd
@@ -2,9 +2,6 @@ extends Sprite2D
 class_name CheckersBoardTop
 
 #var replay = "board:0,2,0,2,0,2,0,2,2,0,2,0,2,0,2,0,0,2,0,2,0,2,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,1,0,0,1,0,1,0,1,0,1,1,0,1,0,1,0,1,0|move:5,2,4,3|board:0,2,0,2,0,2,0,2,2,0,2,0,2,0,2,0,0,2,0,2,0,0,0,2,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,1,0,0,1,0,1,0,1,0,1,1,0,1,0,1,0,1,0";
-var black_king_texture = preload("res://checker_black_king.png")
-var red_king_rexture = preload("res://checker_red_king.png")
-
 var replay = null;
 var redPiece: Sprite2D
 
@@ -20,8 +17,6 @@ var prev_move: Array[Vector2]
 
 var has_connected = false
 
-var player = -1
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var appPlugin := Engine.get_singleton("AppPlugin")
@@ -36,25 +31,13 @@ func _ready() -> void:
 	if replay == null:
 		return
 		
-	player = appPlugin.getPlayer()
-	print("got the player: ", player)
-		
 	redPiece = get_node("CheckerPieceRed")
 	var blackPiece: Sprite2D = get_node("CheckerPieceBlack")
 	boardHighlight = get_node("BoardHighlight")
 	
 	var prevBoard = replay.split('|')[0].substr(6).split(',');
-	
-	var replayMoves = Array()
-	
-	for elem in replay.split('|'):
-		var spl = elem.split(':')
-		if spl[0] == 'move' || spl[0] == 'attack':
-			replayMoves.append(elem)
-	
+	var move = replay.split('|')[1].substr(5).split(',');
 	var nextBoard = replay.split('|')[2].substr(6).split(',');
-	
-	print(replayMoves)
 	
 	for y in range(0, 8):
 		for x in range(0, 8):
@@ -71,18 +54,11 @@ func _ready() -> void:
 				newPiece.visible = true
 				add_child(newPiece)
 				print(newPiece.name, " : ", newPiece.position)
-				
-				if replayMoves.size() > 0:
+				if newPiece.name == move[0] + ',' + move[1]:
+					var newPos = Vector2(redPiece.position.x + (135 * int(move[2])), redPiece.position.y + (135 * (int(move[3])+1)))
 					var tween = newPiece.get_tree().create_tween()
-					for move in replayMoves:
-						var moveType = move.split(':')[0]
-						var movePos = move.split(':')[1].split(',')
-						if newPiece.name == movePos[0] + ',' + movePos[1]:
-							var newPos = Vector2(redPiece.position.x + (135 * int(movePos[2])), redPiece.position.y + (135 * (7 - int(movePos[3]))))
-							tween.tween_property(newPiece, "position", newPos, 1.0).set_trans(Tween.TRANS_SINE)
-							newPiece.name = movePos[2] + "," + movePos[3];
-							if moveType == "attack":
-								jump_piece(int(movePos[0]), int(movePos[1]), int(movePos[2]), int(movePos[3]))
+					tween.tween_property(newPiece, "position", newPos, 1.0).set_trans(Tween.TRANS_SINE)
+					newPiece.name = move[2] + "," + move[3];
 
 func _set_replay(new_replay: String):
 	replay = new_replay
@@ -97,57 +73,23 @@ func export_replay() -> String:
 		for x in range(0, 8):
 			var piece: Sprite2D = get_node_or_null(str(x) + "," + str(7-y))
 			if piece != null:
-				var color = get_piece_color(piece)
-				if color == "red":
+				if piece.texture.resource_path.contains("red"):
 					board[(7 - y) * 8 + x] = "1"
-				elif color == "black":
+				elif piece.texture.resource_path.contains("black"):
 					board[(7 - y) * 8 + x] = "2"
 	
 	var boardStr: String
 	for val in board:
 		boardStr += val + ","
-		
-	var moveType = "move"
-	if abs(prev_move[0].x - prev_move[1].x) > 1:
-		moveType = "attack"
-	
-	if player == 1:
-		player = 2
-	elif player == 2:
-		player = 1
 	
 	return replay.split('|')[2] + "|move:" + str(prev_move[0].x) + "," + str(prev_move[0].y) + "," + str(prev_move[1].x) + "," + str(prev_move[1].y) + "|board:" + boardStr.substr(0, boardStr.length()-1)
-	
-func jump_piece(prevX: int, prevY: int, newX: int, newY: int):
-	var x_step = 1 if newX > prevX else -1
-	var y_step = 1 if newY > prevY else -1
-	var jumpedPiece: Sprite2D = get_node_or_null(str(prevX + x_step) + "," + str(prevY + y_step))
-	if jumpedPiece != null:
-		var tween = jumpedPiece.get_tree().create_tween()
-		var modulate_color = jumpedPiece.self_modulate
-		modulate_color.a = 0.0
-		tween.tween_property(jumpedPiece, "self_modulate", modulate_color, 1.0).set_trans(Tween.TRANS_LINEAR)
-		jumpedPiece.name = "jumped"
+
 
 func move_piece(piece: Sprite2D, x: int, y: int):
 	var newPos = Vector2(redPiece.position.x + (135 * x), redPiece.position.y + (135 * y))
 	var tween = piece.get_tree().create_tween()
 	tween.tween_property(piece, "position", newPos, 0.5).set_trans(Tween.TRANS_SINE)
-	var color = get_piece_color(piece)
-	if (color == "black" and (7-y) == 7) or (color == "red" and (7-y) == 0):
-		tween.tween_callback(set_checker_king.bind(piece, color))
 	piece.name = str(x) + "," + str(7-y)
-
-func set_checker_king(piece: Sprite2D, color: String):
-	if color == "red":
-		piece.texture = red_king_rexture
-	elif color == "black":
-		piece.texture = black_king_texture
-		
-func is_checker_king(piece: Sprite2D) -> bool:
-	if piece.texture.resource_path.contains("king"):
-		return true
-	return false
 
 func add_highlight(x: int, y: int):
 	var newHighlight: BoardHighlight = boardHighlight.duplicate()
@@ -165,38 +107,23 @@ func clear_highlights():
 		highlight.free()
 	highlights.clear()
 
-func get_piece_color(piece: Sprite2D) -> String:
-	if piece.texture.resource_path.contains("red"):
-		return "red"
-	elif piece.texture.resource_path.contains("black"):
-		return "black"
-	return "unknown"
 
 func gen_moves():
 	moves.clear()
 	var diagonals: Array[Vector2]
-	var color = get_piece_color(clicked_piece)
-	var isKing = is_checker_king(clicked_piece)
-	if color == "black" or isKing:
+	var name = clicked_piece.texture.resource_path
+	if name.contains("black"):
 		diagonals.append(Vector2(-1, 1))
 		diagonals.append(Vector2(1, 1))
-	elif color == "red" or isKing:
+	elif name.contains("red"):
 		diagonals.append(Vector2(1, -1));
 		diagonals.append(Vector2(-1, -1));
 		
 	for diagonal in diagonals:
 		var pos = Vector2(clickedPiecePos.x + diagonal.x, clickedPiecePos.y + diagonal.y)
-		var piece = get_node_or_null(str(pos.x) + "," + str(pos.y))
-		if piece == null:
+		if get_node_or_null(str(pos.x) + "," + str(pos.y)) == null:
 			moves.append(pos)
 			add_highlight(pos.x, 7 - pos.y)
-		elif !check_player(piece): #check for jumps
-			var x_step = 1 if pos.x > clickedPiecePos.x else -1
-			var y_step = 1 if pos.y > clickedPiecePos.y else -1
-			var newPos = Vector2(pos.x + x_step, pos.y + y_step)
-			if get_node_or_null(str(newPos.x) + "," + str(newPos.y)) == null:
-				moves.append(newPos)
-				add_highlight(newPos.x, 7 - newPos.y)
 
 
 func undo_move():
@@ -206,13 +133,6 @@ func undo_move():
 	(get_node("../UndoButton") as Button).disabled = true
 	(get_node("../SendButton") as Button).disabled = true
 
-func check_player(piece: Sprite2D) -> bool:
-	var color = get_piece_color(piece)
-	if player == 1 and color == "black":
-		return true
-	elif player == 2 and color == "red":
-		return true
-	return false
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
@@ -223,12 +143,11 @@ func _input(event: InputEvent) -> void:
 			
 			var clickedPiece: Sprite2D = get_node_or_null(str(x) + "," + str(7-y))
 			if clickedPiece != null:
-				if check_player(clickedPiece):
-					clear_highlights()
-					clicked_piece = clickedPiece
-					clickedPiecePos = Vector2(x, 7-y)
-					add_highlight(x, y)
-					gen_moves()
+				clear_highlights()
+				clicked_piece = clickedPiece
+				clickedPiecePos = Vector2(x, 7-y)
+				add_highlight(x, y)
+				gen_moves()
 			elif clicked_piece != null and moves.has(Vector2(x, 7-y)):
 				move_piece(clicked_piece, x, y)
 				clicked_piece = null
@@ -236,8 +155,6 @@ func _input(event: InputEvent) -> void:
 				has_moved = true
 				prev_move.insert(0, clickedPiecePos)
 				prev_move.insert(1, Vector2(x, 7-y))
-				if abs(prev_move[0].x - prev_move[1].x) > 1:
-					jump_piece(prev_move[0].x, prev_move[0].y, prev_move[1].x, prev_move[1].y)
 				(get_node("../UndoButton") as Button).disabled = false
 				(get_node("../SendButton") as Button).disabled = false
 				
